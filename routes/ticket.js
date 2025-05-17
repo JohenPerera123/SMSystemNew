9f5f5df20ce6000e36443eae96cc580d9bfd1586
Johen <pererajohen@gmail.com>
2025-05-17 20:38:03 +0530

    Resolved merge conflict in ticket.js

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,18 +1,11 @@
 import express from 'express';
-<<<<<<< HEAD
 import { bookTicket, getTicketsByEvent, getAllTickets } from '../controllers/ticketController.js';
-=======
-import { bookTicket, getTicketsByEvent, getTicketTrend } from '../controllers/ticketController.js';
->>>>>>> cfac658 (sixth commit)
+import { getTicketTrend } from '../controllers/ticketController.js';
 const router = express.Router();
 
 router.post('/book', bookTicket);
 router.get('/event/:eventId', getTicketsByEvent);
-<<<<<<< HEAD
 router.get('/', getAllTickets)
-=======
 router.get('/trend', getTicketTrend);
-
->>>>>>> cfac658 (sixth commit)
 
 export default router;
